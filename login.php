07219d180b5535aa60fa9833c942e0381c2f3494
Christian Novotny <christian.nvot>
2014-12-10 10:53:01 +0100

    current status

--- login.php
+++ login.php
@@ -3,7 +3,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     session_start();
 
     $_SESSION['angemeldet'] = true;
-    $_SESSION['id'] = $id;
+    //$_SESSION['id'] = $id;
+
+    $hostname = $_SERVER['HTTP_HOST'];
+    $path = dirname($_SERVER['PHP_SELF']);
 
     // Weiterleitung zur geschützten Startseite
     if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1') {
@@ -14,19 +17,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 
-    header('Location: http://' . $hostname . ($path == '/' ? '' : $path) . '/mobilestart.html');
-    exit;
+    header('Location: http://' . $hostname . ($path === '/hci/' ? '' : $path) . '/mobilemenu.html');
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
- <head>
-  <title>Geschützter Bereich</title>
- </head>
- <body>
-     <form action="mobilemenu.php" method="post">
-   <input type="submit" value="Anmelden" />
-  </form>
- </body>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form action="login.php" method="post">
+            <input type="submit" value="Anmelden" />
+        </form>
+        <?php
+        echo $hostname . ($path === '/hci/' ? '' : $path) . '/mobilemenu.html';
+        ?>
+    </body>
 </html>
 
--- mobilemenu.php
+++ mobilemenu.php
@@ -1,22 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     session_start();
-
-    $_SESSION['angemeldet'] = true;
-    $_SESSION['id'] = $id;
-
-    // Weiterleitung zur geschützten Startseite
-    if ($_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1') {
-        if (php_sapi_name() == 'cgi') {
-            header('Status: 303 See Other');
-        } else {
-            header('HTTP/1.1 303 See Other');
-        }
-    }
-
-    header('Location: http://' . $hostname . ($path == '/' ? '' : $path) . '/mobilestart.html');
-    exit;
-}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml2/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
@@ -41,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <body>
 
         <ul id="countrytabs" class="shadetabs">
-            <li><a href="login.php" rel=countrycontainer">Quiz</a></li>
+            <li><a href="quizmobile.php" rel=countrycontainer">Quiz</a></li>
             <li><a href="mobilestart.html" rel="countrycontainer">Foto</a></li>
             <li><a href="external3.htm" rel="countrycontainer">Tab 3</a></li>
             <li><a href="external4.htm" rel="#iframe">Tab 4</a></li>
@@ -54,10 +37,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         <script type="text/javascript">
 
-            var countries = new ddajaxtabs("countrytabs", "countrydivcontainer")
-            countries.setpersist(true)
-            countries.setselectedClassTarget("link") //"link" or "linkparent"
-            countries.init()
+            var countries = new ddajaxtabs("countrytabs", "countrydivcontainer");
+            countries.setpersist(true);
+            countries.setselectedClassTarget("link"); //"link" or "linkparent"
+            countries.init();
 
         </script>
 
