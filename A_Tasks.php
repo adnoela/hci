58ea24be30e4d5e2b0d804e07a908df16ac9f405
adnoela <sonstige_mails@gmx.at>
2014-12-17 12:55:10 +0100

    tasks

---
+++ A_Tasks.php
@@ -0,0 +1,30 @@
+<?php
+
+/* 
+ * 
+*mario*
+hintergrundbild + design public screen
+eingangsnachricht mobilseiten (willkommen zum quiz blablala nur einmal voten)
+endenachricht mobile+public
+weiterleitung mobil quiz->malen (pushernachricht ins index.php)
+
+
+*dani*
+hintergrundbild + design public screen
+eingangsnachricht mobilseiten (willkommen zum quiz blablala nur einmal voten)
+endenachricht mobile+public
+
+
+*chris*
+fotoseite
+hintergrundbild + design public screen
+
+
+*chris+dani*
+content (5 quizfragen + antworten, 5 drawings+antworten)
+userzahlen (pusher 'anmelden', alle paar min server rq ob noch da)
+
+
+
+ */
+
