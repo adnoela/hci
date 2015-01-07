5b606b7dcb76de715f98f4bf518267439e2a3103
adnoela <sonstige_mails@gmx.at>
2015-01-07 01:22:48 +0100

    pic page

--- mobilefoto.php
+++ mobilefoto.php
@@ -8,9 +8,47 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <style>
+            .thumbnail {
+                float: left;
+                width: 45%;
+                height: 378px;
+                margin: 2%;
+            }
+            .pictures {
+                margin-top: 5%;
+            }
+        </style>
         <title></title>
     </head>
     <body>
-        Fotos here.
+        <div class="col-xs-12">
+            <div class="btn-group btn-group-justified" role="group">
+                <div class="btn-group" role="group">
+                    <a href="mobilefoto.php">
+                        <button type="button" class="btn btn-primary">Fotostream</button>
+                    </a>
+                </div>
+                <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-success" autofocus="true">Drawing-Quiz</button>
+                </div>
+            </div>
+        </div>
+        
+        <div class="pictures">
+        <img class="thumbnail" src="pics/markt1.jpg">
+        <img class="thumbnail" src="pics/markt2.jpg">
+        <img class="thumbnail" src="pics/markt3.jpg">
+        <img class="thumbnail" src="pics/markt4.jpg">
+        <img class="thumbnail" src="pics/markt5.jpg">
+        <img class="thumbnail" src="pics/markt6.jpg">
+        <img class="thumbnail" src="pics/markt7.jpg">    
+        </div>
+
     </body>
 </html>
