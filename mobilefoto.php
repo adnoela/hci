585078d39ced16820676ddb6fa4d717dc8a783b3
adnoela <sonstige_mails@gmx.at>
2015-01-07 02:21:06 +0100

    fix foto link

--- mobilefoto.php
+++ mobilefoto.php
@@ -30,24 +30,25 @@ and open the template in the editor.
         <div class="col-xs-12">
             <div class="btn-group btn-group-justified" role="group">
                 <div class="btn-group" role="group">
-                    <a href="mobilefoto.php">
-                        <button type="button" class="btn btn-primary">Fotostream</button>
-                    </a>
+                    <button type="button" class="btn btn-primary">Fotostream</button>
                 </div>
+
                 <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-success" autofocus="true">Drawing-Quiz</button>
+                    <a href="quizmobile.php">
+                        <button type="button" class="btn btn-success" autofocus="true">Drawing-Quiz</button>
+                    </a>
                 </div>
             </div>
         </div>
-        
+
         <div class="pictures">
-        <img class="thumbnail" src="pics/markt1.jpg">
-        <img class="thumbnail" src="pics/markt2.jpg">
-        <img class="thumbnail" src="pics/markt3.jpg">
-        <img class="thumbnail" src="pics/markt4.jpg">
-        <img class="thumbnail" src="pics/markt5.jpg">
-        <img class="thumbnail" src="pics/markt6.jpg">
-        <img class="thumbnail" src="pics/markt7.jpg">    
+            <img class="thumbnail" src="pics/markt1.jpg">
+            <img class="thumbnail" src="pics/markt2.jpg">
+            <img class="thumbnail" src="pics/markt3.jpg">
+            <img class="thumbnail" src="pics/markt4.jpg">
+            <img class="thumbnail" src="pics/markt5.jpg">
+            <img class="thumbnail" src="pics/markt6.jpg">
+            <img class="thumbnail" src="pics/markt7.jpg">    
         </div>
 
     </body>
